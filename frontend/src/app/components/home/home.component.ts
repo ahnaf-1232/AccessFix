2593c1b5070cd4dd205b706bb4ae60d138184031
SadiaTabassum1216 <bsse1216@iit.du.ac.bd>
2024-10-27 20:42:28 +0600

    Frontend designed

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  report: any;
+  title: string = 'AccessFix';
+
+}
